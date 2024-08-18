4a1b51cec9ca7db51f00b4e6164c03a2e0c3a525
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2024-08-18 15:16:22 +0530

    added event manager test

--- Backend/Personal Version/server/nodejs/test/unit/eventSystem/Event.test.ts
+++ Backend/Personal Version/server/nodejs/test/unit/eventSystem/Event.test.ts
@@ -1,9 +1,30 @@
-import { CustomEvent, EventManager } from "../../../src/eventSystem/EventSystem"; // Adjust import path as needed
-import { describe, expect, test } from '@jest/globals';
+import { CustomEvent, EventManager } from "../../../src/eventSystem/EventSystem";
+import { describe, expect, test, beforeEach, afterEach } from '@jest/globals';
+import { sleep } from "../../../src/util/Sleep";
 
 describe('Event System', () => {
-    test('should perform a test', () => {
-        // Replace with actual test logic
-        expect(true).toBe(true);
+    beforeEach(() => {
+        // Before each test
+    });
+
+    afterEach(() => {
+        // Clean up after each test
+    });
+
+    test('should trigger event and handle payload correctly', async () => {
+        class TestEvent extends CustomEvent<string> {
+            constructor(payload: string) {
+                super("TEST_EVENT", payload);
+            }
+        }
+        const payload = "SUP";
+        // Setup event listener
+        EventManager.on(TestEvent, (p) => {
+            //Assert
+            expect(payload).toBe(p);
+        });
+        // Emit the event
+        EventManager.emit(new TestEvent(payload));
+
     });
 });
