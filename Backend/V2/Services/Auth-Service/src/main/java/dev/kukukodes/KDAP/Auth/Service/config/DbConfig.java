19b36044211c1fa156d8bd713aa48cd78ace8d59
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2024-09-08 20:24:21 +0530

    added dev and test profiles

--- Backend/V2/Services/Auth-Service/src/main/java/dev/kukukodes/KDAP/Auth/Service/config/DbConfig.java
+++ Backend/V2/Services/Auth-Service/src/main/java/dev/kukukodes/KDAP/Auth/Service/config/DbConfig.java
@@ -3,20 +3,42 @@ package dev.kukukodes.KDAP.Auth.Service.config;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
-import org.springframework.r2dbc.connection.R2dbcTransactionManager;
-import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.context.annotation.Profile;
 @Configuration
 public class DbConfig {
+    Logger log = LoggerFactory.getLogger(DbConfig.class);
     @Bean
-    public ConnectionFactory connectionFactory() {
+    @Profile("test") //This bean will only be used in test profile
+    public ConnectionFactory h2connectionFactory() {
+        log.info("Creating H2 connection factory");
         // Define the connection factory options
         ConnectionFactoryOptions options = ConnectionFactoryOptions.builder()
                 .option(ConnectionFactoryOptions.DRIVER, "h2")
                 .option(ConnectionFactoryOptions.PROTOCOL, "mem") // In-memory database
                 .option(ConnectionFactoryOptions.DATABASE, "testdb") // Database name
+                .option(ConnectionFactoryOptions.USER, "kuku")
+                .option(ConnectionFactoryOptions.PASSWORD, "kuku")
+                .build();
+
+        // Return the connection factory
+        return ConnectionFactories.get(options);
+    }
+
+    @Bean
+    @Profile("dev") //This bean will only be used in dev profile
+    public ConnectionFactory connectionFactory() {
+        log.info("Creating connection factory");
+        // Define the connection factory options
+        ConnectionFactoryOptions options = ConnectionFactoryOptions.builder()
+                .option(ConnectionFactoryOptions.DRIVER, "h2")
+                .option(ConnectionFactoryOptions.PROTOCOL, "mem") // In-memory database
+                .option(ConnectionFactoryOptions.DATABASE, "devDb") // Database name
                 .option(ConnectionFactoryOptions.USER, "kuku")
                 .option(ConnectionFactoryOptions.PASSWORD, "kuku")
                 .build();
