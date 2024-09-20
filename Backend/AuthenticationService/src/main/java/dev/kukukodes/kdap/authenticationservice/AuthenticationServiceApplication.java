37c333548b9ac6e2e9c4f9d9ce76fbd2c63ffbd8
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2024-09-21 01:17:44 +0530

    added cache and redis dependency

--- Backend/AuthenticationService/src/main/java/dev/kukukodes/kdap/authenticationservice/AuthenticationServiceApplication.java
+++ Backend/AuthenticationService/src/main/java/dev/kukukodes/kdap/authenticationservice/AuthenticationServiceApplication.java
@@ -1,10 +1,14 @@
 package dev.kukukodes.kdap.authenticationservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 
 @SpringBootApplication
+@EnableCaching
+@EnableRabbit
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
