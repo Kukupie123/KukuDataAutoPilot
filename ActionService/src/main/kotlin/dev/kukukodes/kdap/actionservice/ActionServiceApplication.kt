5f3dc84eb7b59c5465b657f04b7aa17c53cda15b
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2024-09-29 02:59:23 +0530

    added action service written in kotlin and reactive webflux

---
+++ ActionService/src/main/kotlin/dev/kukukodes/kdap/actionservice/ActionServiceApplication.kt
@@ -0,0 +1,11 @@
+package dev.kukukodes.kdap.actionservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActionServiceApplication>(*args)
+}
---
+++ ActionService/src/test/kotlin/dev/kukukodes/kdap/actionservice/ActionServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kukukodes.kdap.actionservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
